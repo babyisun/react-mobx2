e638daecd1a1a1369158d01eaec498e9ec59e1c2
北北 <babyisun@qq.com>
2018-12-17 11:59:39 +0800

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,8 @@ const proxy = require('http-proxy-middleware');
 module.exports = (app) => {
   app.use(proxy(
     '/api', {
-      target: 'http://perf.sftcwl.com', // 线上
-      // target: 'http://10.188.40.2:9999',
+      target: 'http://xxx.sftcwl.com', // 线上
+      // target: 'http://1.1.1.1:9999',
       pathRewrite: { '^/api': '' },
       changeOrigin: true,
     },
